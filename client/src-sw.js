c130d3390bbaf5341ab7308221432eaf475ad5a8
unknown <kyujin0704@gmail.com>
2022-08-16 01:11:26 -0400

    update

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies')
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -30,8 +31,10 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 registerRoute( 
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
+    
     cacheName: 'asset-cache',
     plugins: [
+    
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
